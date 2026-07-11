89e5f99c7b12f8803e609bd6a9734a0c10cb96b1
navidzarevscode-debug <YOUR_EMAIL@example.com>
2026-07-11 23:33:34 +0330

    Remove frontend folder from backend repo

--- frontend/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-import path from "path";
-import dotenv from "dotenv";
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
