fb4f08e5df511b1537d2289b2956a97e6fe05da4
navidzarevscode-debug <-->
2026-07-29 02:24:45 +0330

    fix backend chat and cors

--- app/main.py
+++ app/main.py
@@ -31,10 +31,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # اجازه اتصال فرانت‌اند
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:3000",
-    "https://app-nextjs-4g9z0.apps.frk1.abrhapaas.com",
-],
+allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
