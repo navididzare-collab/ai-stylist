b7faae59b21c261358aa74d1f00010ccc25df1ce
navidzarevscode-debug <-->
2026-07-29 09:35:33 +0330

    fix: افزودن کامای جا افتاده در تنظیمات CORS

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # اجازه اتصال فرانت‌اند
 app.add_middleware(
     CORSMiddleware,
-allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
